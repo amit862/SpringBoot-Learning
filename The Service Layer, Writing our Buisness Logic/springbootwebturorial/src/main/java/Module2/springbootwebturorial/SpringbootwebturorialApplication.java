432304e3e5cf051914a3318e1d6afb8f91fa285a
Amit Verma <v99amit@gmail.com>
2026-03-02 02:36:03 +0530

    Commit

---
+++ The Service Layer, Writing our Buisness Logic/springbootwebturorial/src/main/java/Module2/springbootwebturorial/SpringbootwebturorialApplication.java
@@ -0,0 +1,13 @@
+package Module2.springbootwebturorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwebturorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwebturorialApplication.class, args);
+	}
+
+}
